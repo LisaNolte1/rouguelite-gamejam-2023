254ec922f75fda26a824639170770608c6027834
Ivan Von Staden <ivanv@bbd.co.za>
2023-09-02 11:13:43 +0200

    Final changes v2

--- Assets/scripts/Effects/ForcePush.cs
+++ Assets/scripts/Effects/ForcePush.cs
@@ -7,21 +7,6 @@ public class ForcePush : MonoBehaviour, IEffect
     public void ApplyEffect(GameObject player)
     {
 
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        enemies = Array.FindAll(enemies, x => Vector3.Distance(player.transform.position, x.transform.position) < StatManager.getRange() * 2);
-        foreach (GameObject enemy in enemies)
-        {
-
-            Vector3 direction = player.transform.position - enemy.transform.position;
-            direction = Vector3.Normalize(direction);
-            float delta = Vector3.Distance(enemy.transform.position, player.transform.position);
-            delta = StatManager.getRange() - delta;
-            delta *= -1;
-            enemy.transform.position += direction * delta;
-            enemy.GetComponent<EnemyAbstract>().damageEnemy(StatManager.getDamageMultiplyer() * 3);
-
-
-        }
         GameObject visualEffect = Resources.Load<GameObject>("ForcePush");
         Instantiate(visualEffect, player.transform.position, Quaternion.identity);
     }
@@ -31,6 +16,7 @@ public class ForcePush : MonoBehaviour, IEffect
     {
         
     }
+
 
     // Update is called once per frame
     void Update()
